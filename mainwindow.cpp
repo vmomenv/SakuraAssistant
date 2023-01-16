b817b76e466d3123cdc3e47eae29edb8719f7cfb
vmomenv <vmomenv@gmail.com>
2023-01-16 19:45:43 +0800

    设置透明，桌面右下角显示

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,16 +2,23 @@
 #include <QDebug>
 #include<QFrame>
 #include<DTitlebar>
-
 #include<DWidget>
+
 MainWindow::MainWindow(DWidget *parent)
 {
     setFixedSize(230,376);
     titlebar()->setFixedHeight(0);
+    qDebug()<<desktopWidget<<desktopHeight;
+    this->move(desktopWidget-230,desktopHeight-376);
+    setWindowOpacity(0.5);//设置透明
+
 }
+
+//失焦关闭窗口
 void MainWindow::focusOutEvent(QFocusEvent *event){
     close();
 }
+
 
 MainWindow::~MainWindow(){
 
--- mainwindow.h
+++ mainwindow.h
@@ -1,19 +1,25 @@
 #ifndef WIDGET_H
 #define WIDGET_H
-
 #include <DWidget>
-DWIDGET_USE_NAMESPACE
-DWIDGET_USE_NAMESPACE
 #include <DMainWindow>
+#include <QDesktopWidget>
+#include<DApplication>
+DWIDGET_USE_NAMESPACE
 class MainWindow :public DMainWindow
 {
     Q_OBJECT
+
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 private:
+    //读取屏幕信息
+    QDesktopWidget *desktop=DApplication::desktop();
+    int desktopWidget =desktop->width();
+    int desktopHeight = desktop->height();
 
 protected:
     void focusOutEvent(QFocusEvent *event);
+
 };
 #endif // DMAINWINDOW_H
