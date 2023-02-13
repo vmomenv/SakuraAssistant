401f2839b67981f96eba2c8fb6776b680cb63398
vmomenv <vmomenv@gmail.com>
2023-02-13 21:18:00 +0800

    已知connect报错

--- src/todoclassmanager.cpp
+++ src/todoclassmanager.cpp
@@ -96,12 +96,11 @@ void TodoClassManager::loadFromJsonFile(){
     qDebug()<<doc;
     QJsonObject itemObj=doc.object();//将json格式的内容初始化
     itemArray=itemObj.value("items").toArray();//解析items数组,将json存放于itemArray
-    file.close();
 }
 
 
 void TodoClassManager::saveToJsonFile(bool completed,QString name,int i){
-    qDebug()<<name<<completed<<i;
+    //创建文件路径
     QDir home = QDir::home();
     QString configPath = home.filePath(".config/sparkassistant");
     QDir dir(configPath);
@@ -111,41 +110,41 @@ void TodoClassManager::saveToJsonFile(bool completed,QString name,int i){
 
     QString path = dir.filePath("todo.json");
     QFile file(path);
-
+    //将qt中文件复制到~/.config/sparkassistant/
     if (!file.exists()) {
         QFile todoJson(":/res/todo.json");
         todoJson.copy(path);
     }
-    file.open(QIODevice::ReadWrite);
+    //设置文件权限
+    file.setPermissions(QFile::ReadUser | QFile::WriteUser | QFile::ReadGroup | QFile::WriteGroup | QFile::ReadOther | QFile::WriteOther);
+    if(!file.open(QIODevice::ReadWrite)) {
+        qDebug() << "File open error";
+    } else {
+        qDebug() <<"File open!";
+    }
+    qDebug()<<"file路径为"<<path;
+    QByteArray data=file.readAll();//将内容加载到data中
+    QJsonObject smallObj;
+    smallObj.insert("completed", completed);
+    smallObj.insert("name", name);
 
-      if(!file.open(QIODevice::ReadWrite)) {
-          qDebug() << "File open error";
-      } else {
-          qDebug() <<"File open!";
-      }
+    QJsonDocument doc = QJsonDocument::fromJson(data);
+    QJsonObject itemObj = doc.object();
+    QJsonArray itemArr = itemObj["items"].toArray();
+    if(name.size()==0){
+        itemArr.removeAt(i);//删除第i组
+    }else if(i==-1){//新增一条
+        itemArr.append(smallObj);
+    }else{//保存该条
+        itemArr[i]=smallObj;
+    }
 
-      QByteArray data = file.readAll();
-      QJsonObject smallObj;
-      smallObj.insert("completed", completed);
-      smallObj.insert("name", name);
+    itemObj.insert("items", itemArr);
 
-      QJsonDocument doc = QJsonDocument::fromJson(data);
-      QJsonObject itemObj = doc.object();
-      QJsonArray itemArr = itemObj["items"].toArray();
-      if(name.size()==0){
-          itemArr.removeAt(i);//删除第i组
-      }else if(i==-1){//新增一条
-          itemArr.append(smallObj);
-      }else{//保存该条
-          itemArr[i]=smallObj;
-      }
-
-      itemObj.insert("items", itemArr);
-
-      doc.setObject(itemObj);
-      file.resize(0);
-      file.write(doc.toJson());
-      file.close();
+    doc.setObject(itemObj);
+    file.resize(0);
+    file.write(doc.toJson());
+    file.close();
 
 }
 
