ab324331a16d2655fc5a55cbf0bf22dafd46c029
vmomenv <vmomenv@gmail.com>
2023-04-20 21:48:25 +0800

    升级应用后给出提示信息

--- src/sakuraassistant/mainwindow.cpp
+++ src/sakuraassistant/mainwindow.cpp
@@ -219,7 +219,10 @@ void MainWindow::on_sysUpdateButton_clicked(){
                 item->setHidden(true);
             }
         }
+        QMessageBox::warning(this, "提示", "已完成一项更新");
     });
+    QMessageBox::warning(this, "提示", "所选内容已全部更新！");
+
     resultDialog->exec();
 
 
